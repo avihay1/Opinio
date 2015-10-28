6025b8046000d9535d7a4be7d5d9e14c16bfcbf8
Avishay116 <Avishay116@gmail.com>
2015-10-28 22:19:23 +0200

    FacebookAPI.js and pushNotificationsAPI.js Added
    Signed-off-by: Avishay116 <Avishay116@gmail.com>

--- bin/Android/Debug/MainActivity-debug-unaligned.apk.d
+++ bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -2,3 +2,7 @@ C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\ant-build\MainActivity
 C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\ant-build\MainActivity.ap_ \
 C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\ant-build\classes.dex \
 C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\phonegap-facebook-plugin\myappe2785d518e9f4f6ea03055878dd7a400-FacebookLib\ant-build\classes.jar \
+C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\phonegap-facebook-plugin\myappe2785d518e9f4f6ea03055878dd7a400-FacebookLib\libs\bolts-android-1.1.2.jar \
+C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\libs\gcm.jar \
+C:\Users\Avishay\Documents\Git\Opinio\3\platforms\android\libs\android-support-v13.jar \
