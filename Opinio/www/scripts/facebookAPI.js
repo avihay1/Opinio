29e2ec76680f0e4e61d97e34b74d879fdbcfad81
Avishay116 <Avishay116@gmail.com>
2015-10-30 02:17:04 +0200

    Facebook And pushNotification Added

---
+++ Opinio/www/scripts/facebookAPI.js
@@ -0,0 +1,29 @@
+﻿function facebookFunc() {
+    // Check if the user already connected, and collect some data about him
+    facebookConnectPlugin.getLoginStatus(function (response) {
+        alert(response.status);
+        if (response.status === 'connected') {
+            console.log("if");
+            facebookConnectPlugin.api('/me?fields=address,age_range,cover,email,picture.type(large),friends', ["email"],
+            function (response) {
+                console.log(response);
+                alert(response.picture.data.url);
+            });
+        }
+
+        // Else.. login and collect some data about the user
+        else {
+            console.log("else");
+            facebookConnectPlugin.login(["email, public_profile"], function (response) {
+                if (response.authResponse) {
+                    facebookConnectPlugin.api('/me?fields=address,age_range,cover,email,picture.type(large),friends', ["email"],
+                        function (response) {
+                            console.log(response);
+                            alert(response.picture.data.url);
+                        });
+                }
+            });
+        }
+    });                
+    //facebookConnectPlugin.logout(function (response) { alert("LogedOut") }) => For logging out.
+}
